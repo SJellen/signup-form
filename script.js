463a986ceada11a575838c615d7eb17a4a2f1d05
scott jellen <scottjellen@scotts-MBP.fios-router.home>
2020-02-09 16:37:09 -0500

    desktop layout, js done

---
+++ script.js
@@ -0,0 +1,52 @@
+const form = document.getElementById('form')
+const firstName = document.getElementById('firstName')
+const lastName = document.getElementById('lastName')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    checkInputs();
+});
+
+function checkInputs(e) {
+
+    const firstNameID = document.getElementById('firstNameID')
+    const lastNameID = document.getElementById('lastNameID')
+    const emailID = document.getElementById('emailID')
+    const passwordID = document.getElementById('passwordID')
+
+
+    if (firstName.value === '') {
+        firstNameID.classList.add('error')
+    } else {
+        firstNameID.classList.remove('error')
+    }
+
+    if (lastName.value === '') {
+        lastNameID.classList.add('error')
+    } else {
+        lastNameID.classList.remove('error')
+    }
+
+    if (email.value === '') {
+       emailID.classList.add('error')
+    } else if (!emailValid(email.value)){
+        emailID.classList.add('error')
+    }
+    else {
+        firstNameID.classList.remove('error')
+    }
+
+    if (password.value === '') {
+        passwordID.classList.add('error')
+    } else {
+        passwordID.classList.remove('error')
+    }
+
+
+}
+
+function emailValid (email) {
+    return /\S+@\S+\.\S+/.test(email)
+}
